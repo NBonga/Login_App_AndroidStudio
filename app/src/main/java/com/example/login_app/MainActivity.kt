6339a927cfe1a6a532ee42826ce028a85693be44
NBonga <bkubheka25@gmail.com>
2023-05-10 16:31:22 +0200

    Toast to check the register button

--- app/src/main/java/com/example/login_app/MainActivity.kt
+++ app/src/main/java/com/example/login_app/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.login_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var etUsername: EditText
+    lateinit var etPassword: EditText
+    lateinit var btnRegister: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etUsername = findViewById(R.id.etRUserName)
+        etPassword = findViewById(R.id.etRPassword)
+        btnRegister = findViewById(R.id.btnRegister)
+
+        btnRegister.setOnClickListener{
+            val Name = etUsername.getText().toString();
+            Toast.makeText(this, Name + " has been registered", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/login_app/RegisterActivity.kt
+++ app/src/main/java/com/example/login_app/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.example.login_app
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
@@ -18,5 +19,10 @@ class RegisterActivity : AppCompatActivity() {
         etUsername = findViewById(R.id.etRUserName)
         etPassword = findViewById(R.id.etRPassword)
         btnRegister = findViewById(R.id.btnRegister)
+
+        btnRegister.setOnClickListener{
+            Toast.makeText(this, " has been registered", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
