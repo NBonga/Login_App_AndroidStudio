99d983be95939f043349d8ec1cddb6cb49aa5008
Donnedoni Fillies <donnedonifillies@gmail.com>
2023-05-17 10:18:11 +0200

    added the links to the movies on selection two

--- app/src/main/java/com/example/login_app/MovieSelection2.kt
+++ app/src/main/java/com/example/login_app/MovieSelection2.kt
@@ -38,6 +38,26 @@ class MovieSelection2 : AppCompatActivity() {
         imageView4.setOnClickListener {
             openYouTubeVideo("https://www.youtube.com/watch?v=XdKzUbAiswE&pp=ygULdHVybmluZyByZWQ%3D")
         }
+        val imageView5 = findViewById<ImageView>(R.id.shrek)
+        imageView5.setOnClickListener {
+            openYouTubeVideo("https://www.youtube.com/watch?v=CwXOrWvPBPk&pp=ygUNU2hlcmsgdHJhaWxlcg%3D%3D")
+        }
+        val imageView6 = findViewById<ImageView>(R.id.extrac)
+        imageView6.setOnClickListener {
+            openYouTubeVideo("https://www.youtube.com/watch?v=6GRemRH7xFE&pp=ygUUZXh0cmFjdGlvbiAyIHRyYWlsZXI%3D")
+        }
+        val imageView7 = findViewById<ImageView>(R.id.hang)
+        imageView7.setOnClickListener {
+            openYouTubeVideo("https://www.youtube.com/watch?v=tlize92ffnY&pp=ygUUdGhlIGhhbmdvdmVyIHRyYWlsZXI%3D")
+        }
+        val imageView8 = findViewById<ImageView>(R.id.man)
+        imageView8.setOnClickListener {
+            openYouTubeVideo("https://www.youtube.com/watch?v=n6ir-qPI2PU&pp=ygUWdGhlIGxpdHRsZSBtYW4gdHJhaWxlcg%3D%3D")
+        }
+        val imageView9 = findViewById<ImageView>(R.id.per)
+        imageView9.setOnClickListener {
+            openYouTubeVideo("https://www.youtube.com/watch?v=vKCFkFaIwU0&pp=ygUadGhlIHBlcmZlY3QgbWF0Y2ggdHJhaWxlciA%3D")
+        }
     }
 
     private fun openYouTubeVideo(videoUrl: String) {
